34b61a90b3dc98e503fb8c6c18e958ea1b09bbb6
Ihor Kolybaba <kagor00@gmail.com>
2024-02-17 14:52:18 +0200

    add constructor_NullNameParamPassed_ThrowsIllegalArgumentException()

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -5,6 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class HorseTest {
 
     @Test
+    void constructor_NullNameParamPassed_ThrowsIllegalArgumentException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 2));
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @Test
     void getName() {
     }
 
